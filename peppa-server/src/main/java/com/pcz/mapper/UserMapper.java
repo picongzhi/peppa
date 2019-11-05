85324a1c61640d6dbc84a55aa04b7dc94e8f2544
picongzhi <picongzhi@gmail.com>
2019-11-06 00:12:23 +0800

    feat(mybatis): add pagehelper

--- peppa-server/src/main/java/com/pcz/mapper/UserMapper.java
+++ peppa-server/src/main/java/com/pcz/mapper/UserMapper.java
@@ -1,14 +1,12 @@
 package com.pcz.mapper;
 
 import com.pcz.model.User;
+import tk.mybatis.mapper.common.ConditionMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
-
-import java.util.List;
 
 /**
  * @author picongzhi
  */
-public interface UserMapper extends Mapper<User>, MySqlMapper<User> {
-//    List<User> selectAll();
+public interface UserMapper extends Mapper<User>, MySqlMapper<User>, ConditionMapper<User> {
 }
--- peppa-server/src/test/java/cm/pcz/mapper/UserMapperTest.java
+++ peppa-server/src/test/java/cm/pcz/mapper/UserMapperTest.java
@@ -1,15 +1,20 @@
 package cm.pcz.mapper;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.pcz.mapper.UserMapper;
 import com.pcz.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/spring.xml"})
 public class UserMapperTest {
@@ -31,5 +36,25 @@ public class UserMapperTest {
         User user = new User();
         user.setName("pcz");
         userMapper.insertUseGeneratedKeys(user);
+    }
+
+    @Test
+    public void selectWithCondition() {
+        Example example = new Example(User.class);
+        example.createCriteria().andEqualTo("id", 1);
+        List<User> userList = userMapper.selectByExample(example);
+        System.out.println(userList);
+    }
+
+    @Test
+    public void selectByPage() {
+        int page = 2;
+        int size = 3;
+        PageHelper.startPage(page, size);
+        List<User> userList = userMapper.selectAll();
+        PageInfo<User> userPageInfo = new PageInfo<>(userList);
+        log.info("total page: {}", userPageInfo.getPages());
+        log.info("page size: {}", userPageInfo.getSize());
+        userPageInfo.getList().forEach(user -> log.info("user: {}", user));
     }
 }
