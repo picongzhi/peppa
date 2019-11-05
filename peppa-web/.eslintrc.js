e3fc7be6dda049d89d23e38592c34e79e70b2169
picongzhi <picongzhi@gmail.com>
2019-11-05 17:32:05 +0800

    feat(web): add tinymce editor

--- peppa-web/.eslintrc.js
+++ peppa-web/.eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-unused-vars": [2, {
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      "args": "none"
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
--- peppa-web/src/router/index.js
+++ peppa-web/src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'home',
     component: Home
@@ -16,7 +15,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/editor',
+    name: 'editor',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/Editor.vue')
   }
 ]
 
